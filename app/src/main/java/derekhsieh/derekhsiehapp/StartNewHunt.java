9457441c9124a59d29dbab8cb5787a56d8312522
joeyschen <joeyshchen@gmail.com>
2016-04-18 15:01:11 -0700

    added fonts and experimented with changing fonts

--- app/src/main/java/derekhsieh/derekhsiehapp/StartNewHunt.java
+++ app/src/main/java/derekhsieh/derekhsiehapp/StartNewHunt.java
@@ -1,13 +1,18 @@
 package derekhsieh.derekhsiehapp;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
 
 public class StartNewHunt extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView txt = (TextView) findViewById(R.id.login_Title);
+        Typeface font = Typeface.createFromAsset(getAssets(), "Aliquam.ttf");
+        txt.setTypeface(font);
         setContentView(R.layout.activity_start_new_hunt);
     }
 
