312646f10e5b4484805c48d5ba42e1eb3db6e5d2
Joseph Chen <josephchen@Josephs-MacBook-Pro.local>
2016-04-18 17:02:08 -0700

    Made changes to the UI, played with the login font and layout

--- app/src/main/java/derekhsieh/derekhsiehapp/StartNewHunt.java
+++ app/src/main/java/derekhsieh/derekhsiehapp/StartNewHunt.java
@@ -10,9 +10,6 @@ public class StartNewHunt extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView txt = (TextView) findViewById(R.id.login_Title);
-        Typeface font = Typeface.createFromAsset(getAssets(), "Aliquam.ttf");
-        txt.setTypeface(font);
         setContentView(R.layout.activity_start_new_hunt);
     }
 
